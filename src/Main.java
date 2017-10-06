b58c60dc9b2159c277029aeae974f5b8c89b357a
anthonyferreol <anthony.ferreol@gmail.com>
2017-10-06 11:28:10 +0200

    Main launch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by Epulapp on 06/10/2017.
  */
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        //Scene scene = new Scene((new View(primaryStage)).getRoot(), 500, 600);
 
+        primaryStage.setTitle("TP1 ARSIR RECEPTION");
+        //primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
