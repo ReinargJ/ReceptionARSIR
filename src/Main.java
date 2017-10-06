b2387ce5737ab246b36b345c81d5536906663eb7
anthonyferreol <anthony.ferreol@gmail.com>
2017-10-06 11:48:58 +0200

    Main view button

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -7,11 +9,15 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        //Scene scene = new Scene((new View(primaryStage)).getRoot(), 500, 600);
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("View/main.fxml"));
 
         primaryStage.setTitle("TP1 ARSIR RECEPTION");
-        //primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
