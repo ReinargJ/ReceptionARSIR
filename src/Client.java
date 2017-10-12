d831b9e13be4c93785b60a173d0f61d59ee0f242
anthonyferreol <anthony.ferreol@gmail.com>
2017-10-12 13:59:17 +0200

    Fin interface

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Epulapp on 12/10/2017.
+ */
+public class Client {
+    public static String recevoirFichier(String ip, String port, String destination, String fileName) {
+        return "LA";
+    }
+}
